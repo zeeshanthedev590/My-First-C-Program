d86a7cb0e3b162d7fc50c3b72e64b569ea5da6e4
zeeshanthedev590 <62704414+zeeshanthedev590@users.noreply.github.com>
2020-07-28 12:27:29 -0700

    Wow Circle Area Cheker In C
    Check This Out !!! 🔥🔥🔥🔥🔥🔥🔥🔥🔥

---
+++ circle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    float pie = 3.147;
+    int radius, c;
+    printf("Enter The Radius Of The Circle : ");
+    scanf("%d", &radius);
+    c = radius * radius * pie;
+    printf("The Area Of The Circle Will Be  %d", c);
+    return 0;
+}
+
+
+
