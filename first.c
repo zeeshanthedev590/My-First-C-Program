3392faa0a9cf83d93a5366ee2c353aece45ec80b
zeeshanthedev590 <62704414+zeeshanthedev590@users.noreply.github.com>
2020-07-28 06:42:09 -0700

    My First C Program
    This Is Very Good

---
+++ first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// this is my first C Program
+
+/*
+Wow Amazing This is the Best My First Time In C This Is The Best In The World
+*/
+int main()
+{
+    int tom = 3;
+    printf("Hello World");
+    return 0;
+}
+
